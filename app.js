052cce3311e29ffade30967d6593493c81108cfd
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-04-09 17:31:45 +0500

    yu

---
+++ app.js
@@ -0,0 +1,38 @@
+const todoForm = document.getElementById('todo-form');
+const todoList = document.getElementById('todo-list');
+
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Formani boshqa sahifaga yuborishni oldini oladi
+    
+    const todoInput = document.getElementById('todo-input');
+    const todoText = todoInput.value.trim();
+
+    if (todoText !== '') {
+        addTodoItem(todoText);
+        todoInput.value = ''; // Inputni tozalash
+    }
+});
+
+function addTodoItem(todoText) {
+    const todoItem = document.createElement('li');
+    todoItem.className = 'todo-item';
+    todoItem.textContent = todoText;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'O\'chirish';
+    deleteButton.addEventListener('click', function() {
+        todoItem.remove();
+    });
+
+    todoItem.appendChild(deleteButton);
+    todoList.appendChild(todoItem);
+}
+
+
+
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');
+});
